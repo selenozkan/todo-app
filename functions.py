5018ace8f863d9a12a8b426453eb7c1b4e8f2b97
birbaska <selenozkan23@gmail.com>
2025-03-22 23:39:11 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,19 @@
+FILEPATH = 'todos.txt'
+
+
+def get_todos(filepath=FILEPATH):
+    """ Reads a text file and returns a list of to-do items."""
+    with open(filepath, 'r') as file_local:  # when using with, you don't need to close the file
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):  # default parameter should come after non-default one
+    """ Writes to-do items in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+# print(__name__)
+if __name__ == '__main__':
+    print('Hello')
