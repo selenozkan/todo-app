46bd253a22753eab151c76a7185feb807c7002f8
birbaska <selenozkan23@gmail.com>
2025-03-23 12:48:30 +0100

    Implement Add button

--- gui.py
+++ gui.py
@@ -2,10 +2,26 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key='todo')
 add_button = sg.Button("Add")
 
-# when placed in the same list, they will be shown in the same row.
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
+# each list of the outer list layout represents a row.
+# so, when placed in the same list, they will be shown in the same row.
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+
+        case sg.WIN_CLOSED:
+            break
 window.close()
